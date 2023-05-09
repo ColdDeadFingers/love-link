e47f02d3e365233f2132fc9992614e86110796b1
mayowa makanjuola <mayowa.makanjuola@flutterwavego.com>
2023-05-09 19:35:38 +0100

    updated logo

--- src/components/Display.js
+++ src/components/Display.js
@@ -15,7 +15,7 @@ export default function Display() {
           </div>    
 
         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">@elonmosquite & @fanboi</dt>
+            <dt className="text-sm font-medium leading-6 text-gray-900">@elonmosquito & @fanboi</dt>
             <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">2 years and 7 months</dd>
           </div>  
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import {auth, googleProvider, twitterProvider} from "../config/firebase"
 import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
+import lovelink from "../assets/love-link.png"
 
 export default function Hero() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -22,10 +23,10 @@ export default function Hero() {
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">Love Link</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src={lovelink}
                 alt=""
               />
             </a>
